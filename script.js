3bd2ef389e63bdfb05f0fefb954ffbefeb59c7c8
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-02-26 20:09:10 +0200

    Implement new book and remove book

--- script.js
+++ script.js
@@ -1,42 +1,81 @@
 const myLibrary = [];
+const modal = document.querySelector('.modal');
 const booksContainer = document.querySelector('.books-container');
-
+const addNewBookButton = document.querySelector('.new-book-btn');
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not yet read'}`;
-  };
 }
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not yet read'}`;
+};
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function removeBookFromLibrary(bookNode) {
+  const { index } = bookNode.dataset;
+  myLibrary.splice(index, 1);
+  bookNode.classList.add('transparent');
+  setTimeout(() => populateLibrary(), 350);
+}
+
+function toggleReadStatus(readBtn) {
+  const btn = readBtn;
+  const { index } = btn.parentNode.dataset;
+  myLibrary[index].toggleRead();
+  btn.textContent = myLibrary[index].read ? 'Read' : 'Unread';
+}
+
 function populateLibrary() {
+  while (booksContainer.firstChild) {
+    const currentBook = booksContainer.firstChild;
+    booksContainer.removeChild(currentBook);
+  }
+
   for (let i = 0; i < myLibrary.length; i += 1) {
     const current = myLibrary[i];
     const bookCard = document.createElement('div');
     const title = document.createElement('h2');
     const author = document.createElement('h3');
     const pages = document.createElement('p');
-    const read = document.createElement('p');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
 
     title.textContent = current.title;
     author.textContent = current.author;
     pages.textContent = current.pages;
-    read.textContent = current.read;
+    readBtn.textContent = current.read ? 'Read' : 'Unread';
+    removeBtn.textContent = '×';
 
     bookCard.classList.add('book-card');
+    bookCard.dataset.index = i;
     bookCard.appendChild(title);
+    bookCard.appendChild(removeBtn).classList.add('remove-book');
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
-    bookCard.appendChild(read);
-
+    bookCard.appendChild(readBtn).classList.add('read-btn');
+    readBtn.addEventListener('click', (e) => toggleReadStatus(e.target));
+    removeBtn.addEventListener('click', (e) => removeBookFromLibrary(e.target.parentNode));
     booksContainer.appendChild(bookCard);
   }
+}
+
+function showFormModal() {
+  modal.classList.remove('hidden');
+}
+
+function hideFormModal() {
+  modal.classList.add('hidden');
+  modal.querySelector('form').reset();
 }
 
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
@@ -54,3 +93,31 @@ addBookToLibrary(book5);
 addBookToLibrary(book6);
 
 populateLibrary();
+
+addNewBookButton.addEventListener('click', () => showFormModal());
+
+const submitBtn = document.querySelector('.modal button[type="submit"]');
+const cancelBtn = document.querySelector('.modal button[type="reset"]');
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const newBook = new Book(
+    e.target.form.title.value,
+    e.target.form.author.value,
+    e.target.form.pages.value,
+    e.target.form.read.checked,
+  );
+  addBookToLibrary(newBook);
+  populateLibrary();
+  hideFormModal();
+});
+
+cancelBtn.addEventListener('click', () => {
+  hideFormModal();
+});
+
+modal.addEventListener('click', (e) => {
+  if (!e.target.closest('.form-container')) {
+    hideFormModal();
+  }
+});
