f7a8e29e5fcc7021b252bf972e9ea5189bf8f8d4
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-02-26 15:59:54 +0200

    Implement addBookToLibrary
    Add 6 books to already be present in the library

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hi');
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not yet read'}`;
+  };
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const book2 = new Book('To Kill a Mockingbird', 'Harper Lee', 281, true);
+const book3 = new Book('Pride and Prejudice', 'Jane Austen', 435, false);
+const book4 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+const book5 = new Book('Wuthering Heights', 'Emily Bronte', 357, true);
+const book6 = new Book('1984', 'George Orwell', 328, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
+addBookToLibrary(book6);
+
+console.log(myLibrary);
