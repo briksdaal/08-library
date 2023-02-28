c318da73e7eaa6d5c9f922d29e10279cad3807ab
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-02-28 17:47:19 +0200

    Fix remove and change read status
    All previous functionality now restored except add new book

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ class Library {
     this.myLibrary.splice(index, 1);
   }
 
+  toggleReadStatus(index) {
+    this.myLibrary[index].toggleRead();
+  }
+
   static createBook(title, author, pages, read) {
     let isRead = read;
 
@@ -32,10 +36,79 @@ class Library {
 }
 
 const ScreenController = (() => {
+  const booksContainer = document.querySelector('.books-container');
   const lib = new Library();
   const book1 = Library.createBook('The Hobbit', 'J.R.R. Tolkien', 295, false);
+  const book2 = Library.createBook('To Kill a Mockingbird', 'Harper Lee', 281, true);
+  const book3 = Library.createBook('Pride and Prejudice', 'Jane Austen', 435, false);
+  const book4 = Library.createBook('The Great Gatsby', 'F. Scott Fitzgerald', 180, true);
+  const book5 = Library.createBook('Wuthering Heights', 'Emily Bronte', 357, true);
+  const book6 = Library.createBook('1984', 'George Orwell', 328, false);
+
   lib.addBookToLibrary(book1);
-  console.log(lib.libraryArray);
+  lib.addBookToLibrary(book2);
+  lib.addBookToLibrary(book3);
+  lib.addBookToLibrary(book4);
+  lib.addBookToLibrary(book5);
+  lib.addBookToLibrary(book6);
+
+  const populateScreen = () => {
+    booksContainer.innerHTML = '';
+    for (let i = 0; i < lib.libraryArray.length; i += 1) {
+      const current = lib.libraryArray[i];
+      const bookCard = document.createElement('div');
+      const title = document.createElement('h2');
+      const by = document.createElement('span');
+      const author = document.createElement('h3');
+      const pages = document.createElement('p');
+      const readBtn = document.createElement('button');
+      const removeBtn = document.createElement('button');
+
+      title.textContent = current.title;
+      by.textContent = 'by ';
+      author.textContent = current.author;
+      pages.textContent = `${current.pages} pages`;
+      readBtn.textContent = current.getRead() ? 'Read' : 'Unread';
+      removeBtn.textContent = '×';
+
+      bookCard.classList.add('book-card');
+      bookCard.dataset.index = i;
+      const rowDiv = document.createElement('div');
+      bookCard.appendChild(title);
+      bookCard.appendChild(removeBtn).classList.add('remove-book');
+      rowDiv.appendChild(by);
+      rowDiv.appendChild(author);
+      bookCard.appendChild(rowDiv);
+      bookCard.appendChild(pages);
+      bookCard.appendChild(readBtn).classList.add('read-btn');
+
+      booksContainer.appendChild(bookCard);
+    }
+  };
+
+  function ClickHandlerBooks(e) {
+    if (e.target.tagName !== 'BUTTON') {
+      return;
+    }
+
+    if (e.target.classList.contains('read-btn')) {
+      lib.toggleReadStatus(e.target.parentNode.dataset.index);
+    }
+
+    if (e.target.classList.contains('remove-book')) {
+      lib.removeBookFromLibrary(e.target.parentNode.dataset.index);
+    }
+
+    populateScreen();
+  }
+
+  function ClickHandlerModal(e) {
+
+  }
+
+  booksContainer.addEventListener('click', ClickHandlerBooks);
+
+  populateScreen();
 })();
 
 const myLibrary = [];
@@ -43,7 +116,7 @@ const modal = document.querySelector('.modal');
 const booksContainer = document.querySelector('.books-container');
 const addNewBookButton = document.querySelector('.new-book-btn');
 
-populateLibrary();
+// populateLibrary();
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
